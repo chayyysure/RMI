b2f8ad1c840de79d508d1e02a3090db38f8e7b12
chaitanyasure <chaitu2k3@gmail.com>
2025-09-16 19:50:06 +0530

    RMI implementation

--- src/Remote_Interface.java
+++ src/Remote_Interface.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Remote_Interface {
+package Rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Remote_Interface extends Remote {
+    public double getSquare(double input) throws RemoteException;
+    public double getSquareRoot(double input) throws RemoteException;
+
 }
